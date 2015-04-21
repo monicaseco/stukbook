b41ec629edbaee7408b8fcb070e6f1e70f6b533a
Monica Seco <seco.monica@gmail.com>
2015-04-21 12:07:57 +0200

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "monicaseco", email: "seco.monica@gmail.com", password: "monica31", password_confirmation: "monica31")
+User.create(username: "monicaseco1", email: "seco1.monica@gmail.com", password: "monica31", password_confirmation: "monica31")
+User.create(username: "monicaseco2", email: "seco2.monica@gmail.com", password: "monica31", password_confirmation: "monica31")
+User.create(username: "monicaseco3", email: "seco3.monica@gmail.com", password: "monica31", password_confirmation: "monica31")
+User.create(username: "monicaseco4", email: "seco4.monica@gmail.com", password: "monica31", password_confirmation: "monica31")
+p "Test users created"
